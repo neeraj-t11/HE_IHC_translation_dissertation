33bfb2dc93cf25f1f3761763285d6a4ee1bffe52
NEERAJ RAJESH THOSAR <psxnt2@carey.cs.nott.ac.uk>
2024-08-24 20:24:46 +0100

    Changed the ResNet network, modified layers to introduce dilated convolutions, leaky ReLU, spatial attention

--- models/networks.py
+++ models/networks.py
@@ -350,23 +350,111 @@ def cal_gradient_penalty(netD, real_data, fake_data, device, type='mixed', const
         return 0.0, None
 
 
-class ResnetGenerator(nn.Module):
-    """Resnet-based generator that consists of Resnet blocks between a few downsampling/upsampling operations.
+class SpatialAttention(nn.Module):
+    """Spatial Attention Layer"""
 
-    We adapt Torch code and idea from Justin Johnson's neural style transfer project(https://github.com/jcjohnson/fast-neural-style)
-    """
-
-    def __init__(self, input_nc, output_nc, ngf=64, norm_layer=nn.BatchNorm2d, use_dropout=False, n_blocks=6, padding_type='reflect'):
-        """Construct a Resnet-based generator
+    def __init__(self, kernel_size=7):
+        """Initialize the Spatial Attention layer with a convolutional layer.
 
         Parameters:
-            input_nc (int)      -- the number of channels in input images
-            output_nc (int)     -- the number of channels in output images
-            ngf (int)           -- the number of filters in the last conv layer
-            norm_layer          -- normalization layer
-            use_dropout (bool)  -- if use dropout layers
-            n_blocks (int)      -- the number of ResNet blocks
-            padding_type (str)  -- the name of padding layer in conv layers: reflect | replicate | zero
+            kernel_size (int) -- size of the convolutional kernel (default: 7)
+        """
+        super(SpatialAttention, self).__init__()
+        padding = (kernel_size - 1) // 2
+        self.conv1 = nn.Conv2d(2, 1, kernel_size, padding=padding, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        """Forward function for spatial attention.
+
+        Parameters:
+            x (tensor) -- input feature maps
+
+        Returns the feature maps weighted by the spatial attention.
+        """
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x = torch.cat([avg_out, max_out], dim=1)
+        x = self.conv1(x)
+        return x * self.sigmoid(x)
+
+
+# class ResnetGenerator(nn.Module):
+#     """Resnet-based generator that consists of Resnet blocks between a few downsampling/upsampling operations.
+
+#     We adapt Torch code and idea from Justin Johnson's neural style transfer project(https://github.com/jcjohnson/fast-neural-style)
+#     """
+
+#     def __init__(self, input_nc, output_nc, ngf=64, norm_layer=nn.BatchNorm2d, use_dropout=False, n_blocks=6, padding_type='reflect'):
+#         """Construct a Resnet-based generator
+
+#         Parameters:
+#             input_nc (int)      -- the number of channels in input images
+#             output_nc (int)     -- the number of channels in output images
+#             ngf (int)           -- the number of filters in the last conv layer
+#             norm_layer          -- normalization layer
+#             use_dropout (bool)  -- if use dropout layers
+#             n_blocks (int)      -- the number of ResNet blocks
+#             padding_type (str)  -- the name of padding layer in conv layers: reflect | replicate | zero
+#         """
+#         assert(n_blocks >= 0)
+#         super(ResnetGenerator, self).__init__()
+#         if type(norm_layer) == functools.partial:
+#             use_bias = norm_layer.func == nn.InstanceNorm2d
+#         else:
+#             use_bias = norm_layer == nn.InstanceNorm2d
+
+#         model = [nn.ReflectionPad2d(3),
+#                  nn.Conv2d(input_nc, ngf, kernel_size=7, padding=0, bias=use_bias),
+#                  norm_layer(ngf),
+#                  nn.ReLU(True)]
+
+#         n_downsampling = 2
+#         for i in range(n_downsampling):  # add downsampling layers
+#             mult = 2 ** i
+#             model += [nn.Conv2d(ngf * mult, ngf * mult * 2, kernel_size=3, stride=2, padding=1, bias=use_bias),
+#                       norm_layer(ngf * mult * 2),
+#                       nn.ReLU(True)]
+
+#         mult = 2 ** n_downsampling
+#         for i in range(n_blocks):       # add ResNet blocks
+
+#             model += [ResnetBlock(ngf * mult, padding_type=padding_type, norm_layer=norm_layer, use_dropout=use_dropout, use_bias=use_bias)]
+
+#         for i in range(n_downsampling):  # add upsampling layers
+#             mult = 2 ** (n_downsampling - i)
+#             model += [nn.ConvTranspose2d(ngf * mult, int(ngf * mult / 2),
+#                                          kernel_size=3, stride=2,
+#                                          padding=1, output_padding=1,
+#                                          bias=use_bias),
+#                       norm_layer(int(ngf * mult / 2)),
+#                       nn.ReLU(True)]
+#         model += [nn.ReflectionPad2d(3)]
+#         model += [nn.Conv2d(ngf, output_nc, kernel_size=7, padding=0)]
+#         model += [nn.Tanh()]
+
+#         self.model = nn.Sequential(*model)
+
+#     def forward(self, input):
+#         """Standard forward"""
+#         return self.model(input)
+
+
+
+class ResnetGenerator(nn.Module):
+    """Resnet-based generator that consists of Resnet blocks with additional features."""
+
+    def __init__(self, input_nc, output_nc, ngf=64, norm_layer=nn.BatchNorm2d, use_dropout=False, n_blocks=6, padding_type='reflect'):
+        """Construct a Resnet-based generator with dilated convolutions, spatial attention, and Leaky ReLU.
+
+        Parameters:
+            input_nc (int)             -- the number of channels in input images
+            output_nc (int)            -- the number of channels in output images
+            ngf (int)                  -- the number of filters in the last conv layer
+            norm_layer                 -- normalization layer
+            use_dropout (bool)         -- if use dropout layers
+            n_blocks (int)             -- the number of ResNet blocks
+            padding_type (str)         -- the name of padding layer in conv layers: reflect | replicate | zero
         """
         assert(n_blocks >= 0)
         super(ResnetGenerator, self).__init__()
@@ -388,9 +476,13 @@ class ResnetGenerator(nn.Module):
                       nn.ReLU(True)]
 
         mult = 2 ** n_downsampling
-        for i in range(n_blocks):       # add ResNet blocks
-
-            model += [ResnetBlock(ngf * mult, padding_type=padding_type, norm_layer=norm_layer, use_dropout=use_dropout, use_bias=use_bias)]
+        for i in range(n_blocks):  # add ResNet blocks with new features
+            dilation = 2 if i % 2 == 0 else 1  # Apply dilation to every other block
+            use_spatial_attention = (i % 3 == 0)  # Apply spatial attention to every third block
+            use_leaky_relu = (i % 2 == 0)  # Use Leaky ReLU in some blocks
+            model += [ResnetBlock(ngf * mult, padding_type=padding_type, norm_layer=norm_layer, 
+                                  use_dropout=use_dropout, use_bias=use_bias, dilation=dilation, 
+                                  use_spatial_attention=use_spatial_attention, use_leaky_relu=use_leaky_relu)]
 
         for i in range(n_downsampling):  # add upsampling layers
             mult = 2 ** (n_downsampling - i)
@@ -411,44 +503,123 @@ class ResnetGenerator(nn.Module):
         return self.model(input)
 
 
+# class ResnetBlock(nn.Module):
+#     """Define a Resnet block"""
+
+#     def __init__(self, dim, padding_type, norm_layer, use_dropout, use_bias):
+#         """Initialize the Resnet block
+
+#         A resnet block is a conv block with skip connections
+#         We construct a conv block with build_conv_block function,
+#         and implement skip connections in <forward> function.
+#         Original Resnet paper: https://arxiv.org/pdf/1512.03385.pdf
+#         """
+#         super(ResnetBlock, self).__init__()
+#         self.conv_block = self.build_conv_block(dim, padding_type, norm_layer, use_dropout, use_bias)
+
+#     def build_conv_block(self, dim, padding_type, norm_layer, use_dropout, use_bias):
+#         """Construct a convolutional block.
+
+#         Parameters:
+#             dim (int)           -- the number of channels in the conv layer.
+#             padding_type (str)  -- the name of padding layer: reflect | replicate | zero
+#             norm_layer          -- normalization layer
+#             use_dropout (bool)  -- if use dropout layers.
+#             use_bias (bool)     -- if the conv layer uses bias or not
+
+#         Returns a conv block (with a conv layer, a normalization layer, and a non-linearity layer (ReLU))
+#         """
+#         conv_block = []
+#         p = 0
+#         if padding_type == 'reflect':
+#             conv_block += [nn.ReflectionPad2d(1)]
+#         elif padding_type == 'replicate':
+#             conv_block += [nn.ReplicationPad2d(1)]
+#         elif padding_type == 'zero':
+#             p = 1
+#         else:
+#             raise NotImplementedError('padding [%s] is not implemented' % padding_type)
+
+#         conv_block += [nn.Conv2d(dim, dim, kernel_size=3, padding=p, bias=use_bias), norm_layer(dim), nn.ReLU(True)]
+#         if use_dropout:
+#             conv_block += [nn.Dropout(0.5)]
+
+#         p = 0
+#         if padding_type == 'reflect':
+#             conv_block += [nn.ReflectionPad2d(1)]
+#         elif padding_type == 'replicate':
+#             conv_block += [nn.ReplicationPad2d(1)]
+#         elif padding_type == 'zero':
+#             p = 1
+#         else:
+#             raise NotImplementedError('padding [%s] is not implemented' % padding_type)
+#         conv_block += [nn.Conv2d(dim, dim, kernel_size=3, padding=p, bias=use_bias), norm_layer(dim)]
+
+#         return nn.Sequential(*conv_block)
+
+#     def forward(self, x):
+#         """Forward function (with skip connections)"""
+#         out = x + self.conv_block(x)  # add skip connections
+#         return out
+
+
 class ResnetBlock(nn.Module):
-    """Define a Resnet block"""
+    """Define a ResNet block with dilated convolutions, optional spatial attention, and Leaky ReLU."""
 
-    def __init__(self, dim, padding_type, norm_layer, use_dropout, use_bias):
-        """Initialize the Resnet block
-
-        A resnet block is a conv block with skip connections
-        We construct a conv block with build_conv_block function,
-        and implement skip connections in <forward> function.
-        Original Resnet paper: https://arxiv.org/pdf/1512.03385.pdf
-        """
-        super(ResnetBlock, self).__init__()
-        self.conv_block = self.build_conv_block(dim, padding_type, norm_layer, use_dropout, use_bias)
-
-    def build_conv_block(self, dim, padding_type, norm_layer, use_dropout, use_bias):
-        """Construct a convolutional block.
+    def __init__(self, dim, padding_type, norm_layer, use_dropout, use_bias, dilation=1, use_spatial_attention=False, use_leaky_relu=False):
+        """Initialize the Resnet block with additional features.
 
         Parameters:
-            dim (int)           -- the number of channels in the conv layer.
-            padding_type (str)  -- the name of padding layer: reflect | replicate | zero
-            norm_layer          -- normalization layer
-            use_dropout (bool)  -- if use dropout layers.
-            use_bias (bool)     -- if the conv layer uses bias or not
+            dim (int)                  -- the number of channels in the conv layer
+            padding_type (str)         -- the name of padding layer: reflect | replicate | zero
+            norm_layer                 -- normalization layer
+            use_dropout (bool)         -- if use dropout layers
+            use_bias (bool)            -- if the conv layer uses bias or not
+            dilation (int)             -- dilation rate for dilated convolutions
+            use_spatial_attention (bool)-- if True, adds spatial attention after the conv block
+            use_leaky_relu (bool)      -- if True, uses Leaky ReLU instead of ReLU
+        """
+        super(ResnetBlock, self).__init__()
+        self.use_spatial_attention = use_spatial_attention
 
-        Returns a conv block (with a conv layer, a normalization layer, and a non-linearity layer (ReLU))
+        self.conv_block = self.build_conv_block(dim, padding_type, norm_layer, use_dropout, use_bias, dilation, use_leaky_relu)
+
+        if self.use_spatial_attention:
+            self.spatial_attention = SpatialAttention()
+
+    def build_conv_block(self, dim, padding_type, norm_layer, use_dropout, use_bias, dilation, use_leaky_relu):
+        """Construct a convolutional block with optional dilation and Leaky ReLU.
+
+        Parameters:
+            dim (int)                  -- the number of channels in the conv layer
+            padding_type (str)         -- the name of padding layer: reflect | replicate | zero
+            norm_layer                 -- normalization layer
+            use_dropout (bool)         -- if use dropout layers
+            use_bias (bool)            -- if the conv layer uses bias or not
+            dilation (int)             -- dilation rate for dilated convolutions
+            use_leaky_relu (bool)      -- if True, uses Leaky ReLU instead of ReLU
+
+        Returns a conv block with optional features.
         """
         conv_block = []
         p = 0
         if padding_type == 'reflect':
-            conv_block += [nn.ReflectionPad2d(1)]
+            conv_block += [nn.ReflectionPad2d(dilation)]
         elif padding_type == 'replicate':
-            conv_block += [nn.ReplicationPad2d(1)]
+            conv_block += [nn.ReplicationPad2d(dilation)]
         elif padding_type == 'zero':
-            p = 1
+            p = dilation
         else:
             raise NotImplementedError('padding [%s] is not implemented' % padding_type)
 
-        conv_block += [nn.Conv2d(dim, dim, kernel_size=3, padding=p, bias=use_bias), norm_layer(dim), nn.ReLU(True)]
+        conv_block += [nn.Conv2d(dim, dim, kernel_size=3, padding=p, dilation=dilation, bias=use_bias),
+                       norm_layer(dim)]
+        
+        if use_leaky_relu:
+            conv_block += [nn.LeakyReLU(0.2, True)]
+        else:
+            conv_block += [nn.ReLU(True)]
+
         if use_dropout:
             conv_block += [nn.Dropout(0.5)]
 
@@ -461,14 +632,24 @@ class ResnetBlock(nn.Module):
             p = 1
         else:
             raise NotImplementedError('padding [%s] is not implemented' % padding_type)
-        conv_block += [nn.Conv2d(dim, dim, kernel_size=3, padding=p, bias=use_bias), norm_layer(dim)]
+
+        conv_block += [nn.Conv2d(dim, dim, kernel_size=3, padding=p, dilation=1, bias=use_bias),
+                       norm_layer(dim)]
 
         return nn.Sequential(*conv_block)
 
     def forward(self, x):
-        """Forward function (with skip connections)"""
-        out = x + self.conv_block(x)  # add skip connections
-        return out
+        """Forward function with skip connection and optional spatial attention.
+
+        Parameters:
+            x (tensor) -- input tensor
+
+        Returns the tensor after passing through the ResNet block.
+        """
+        out = self.conv_block(x)
+        if self.use_spatial_attention:
+            out = out * self.spatial_attention(out)
+        return x + out  # Skip connection
 
 
 class UnetGenerator(nn.Module):
